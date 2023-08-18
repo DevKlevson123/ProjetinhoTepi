47234a583a9eadeed128665ed66dc2b233c26c90
jennyferjacinto <jjs11@aluno.ifal.edu.br>
2023-08-18 11:21:14 -0300

    Atualização de estilazação do Picker

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,35 +1,43 @@
 import React, { useState }  from 'react';
 import { StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
-import Slider from '@react-native-community/slider';
 import { Ionicons } from 'react-native-vector-icons';
 import { Picker } from '@react-native-picker/picker';
 
 export default function Form(props){
-    const[Satisfacao, setSatisfacao] = useState('')
+    const[nomeAtividade, setnomeAtividade] = useState('')
     const[descricaoAtividade, setdescriçãoAtvidade] = useState('')
+    const[Satisfacao, setSatisfacao] = useState('')
 
     return(
       <View style={styles.container}>
         <View style={styles.Modal}>
           <Text style={styles.Text}>Nome Atividade</Text>
-          <TextInput placeholder='Nome Atividade' style={styles.Input}></TextInput>
+          <TextInput 
+            placeholder='Nome Atividade' 
+            onChangeText={(texto) => setnomeAtividade(texto)}
+            style={styles.Input}
+            ></TextInput>
+
           <Text style={styles.Text}>descrição da Atividade </Text>
           <TextInput 
           placeholder='descrição Atividade' 
           style={styles.Input}
           onChangeText={(texto) => setdescriçãoAtvidade(texto)}
           ></TextInput>
+
           <Text style={styles.Text}>Satifação</Text>
-          <Picker 
-            style={styles.Picker}
-            selectedValue={Satisfacao}
-            onValueChange={(intemValue) => setSatisfacao(intemValue)}
-          >
-            <Picker.Item label='Ruim' value='Ruim'/>
-            <Picker.Item label='Médio' value='Médio'/>
-            <Picker.Item label='Bom' value='Bom'/>
-            <Picker.Item label='Muito Bom' value='Muito Bom'/>
-          </Picker>
+          <View style={styles.Picker}>
+            <Picker 
+              style={{width: 200, height: 50}}
+              selectedValue={Satisfacao}
+              onValueChange={(intemValue) => setSatisfacao(intemValue)}
+            >
+              <Picker.Item label='Ruim' value='Ruim'/>
+              <Picker.Item label='Médio' value='Médio'/>
+              <Picker.Item label='Bom' value='Bom'/>
+              <Picker.Item label='Muito Bom' value='Muito Bom'/>
+            </Picker>
+          </View>
 
           <TouchableOpacity
           onPress={ props.fechar}
@@ -75,6 +83,7 @@ const styles = StyleSheet.create({
       alignItems: 'flex-start',
       marginLeft: 14,
       backgroundColor: '#fff',
+      color: '#000',
       width: 200,
       borderRadius: 2000,
     }
